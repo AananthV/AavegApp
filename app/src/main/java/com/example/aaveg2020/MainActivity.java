b9e55c2602b5e4a6b20a359a1522a2b69981f194
Avi Kumar Singh <avithewinner1508@gmail.com>
2020-01-29 00:37:06 +0000

    [FEAT]Home Screen with horizontal navs

--- app/src/main/java/com/example/aaveg2020/MainActivity.java
+++ app/src/main/java/com/example/aaveg2020/MainActivity.java
@@ -7,11 +7,31 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.example.aaveg2020.Home.HomeFragment;
+import androidx.viewpager.widget.ViewPager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import android.view.View;
+
+import com.example.aaveg2020.Scoreboard.ScoreboardFragment;
+import com.example.aaveg2020.login.LoginActivity;
+import com.google.android.material.tabs.TabLayout;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
 import com.example.aaveg2020.events.ClustersFragment;
 
 public class MainActivity extends AppCompatActivity implements FragmentChangeListener {
 
+    TabLayout mainScreenTabLayout;
+    ViewPager mainScreenViewPager;
+    ViewPagerAdapterHome adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,14 @@ public class MainActivity extends AppCompatActivity implements FragmentChangeLis
         FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.main_framelayout,new HomeFragment());
         fragmentTransaction.commit();
+        mainScreenTabLayout = (TabLayout) findViewById(R.id.tab_layout_main_screen);
+        mainScreenViewPager = (ViewPager) findViewById(R.id.view_pager_main_screen);
+        adapter = new ViewPagerAdapterHome(getSupportFragmentManager(),BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mainScreenViewPager.setAdapter(adapter);
+        mainScreenTabLayout.setupWithViewPager(mainScreenViewPager);
+
+        mainScreenTabLayout.getTabAt(2).select();
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/aaveg2020/ViewPagerAdapterHome.java
@@ -0,0 +1,69 @@
+package com.example.aaveg2020;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.aaveg2020.fragments.AboutUsFragment;
+import com.example.aaveg2020.fragments.EventsFragment;
+import com.example.aaveg2020.fragments.HomeFragment;
+import com.example.aaveg2020.fragments.LeaderboardFragment;
+import com.example.aaveg2020.fragments.SponsorsFragment;
+
+public class ViewPagerAdapterHome extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapterHome(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment f;
+        switch (position) {
+            case 0 :
+                f= new AboutUsFragment();
+                break;
+            case 1 :
+                f = new EventsFragment();
+                break;
+            case 2 :
+                f = new HomeFragment();
+                break;
+            case 3 :
+                f = new LeaderboardFragment();
+                break;
+            case 4 :
+                f = new SponsorsFragment();
+                break;
+            default :
+                f= new AboutUsFragment();
+        }
+        return f;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        switch (position) {
+            case 0 :
+                return "A";
+            case 1 :
+                return "E";
+            case 2 :
+                return "H";
+            case 3 :
+                return "L";
+            case 4 :
+                return "S";
+            default :
+                return "A";
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}

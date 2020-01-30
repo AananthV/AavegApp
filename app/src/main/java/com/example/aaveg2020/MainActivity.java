639d46eeb944d67824468c3ea71d44aae419feb4
shashank-07 <shash07@live.com>
2020-01-30 17:11:04 +0530

    [FEAT] Add assets to login and hostel views, Add basic notifications

--- app/src/main/java/com/example/aaveg2020/MainActivity.java
+++ app/src/main/java/com/example/aaveg2020/MainActivity.java
@@ -1,40 +1,19 @@
 package com.example.aaveg2020;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.example.aaveg2020.Home.HomeFragment;
-import com.example.aaveg2020.Scoreboard.ScoreboardFragment;
-import com.example.aaveg2020.login.LoginActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        {
-            NotificationChannel channel=new NotificationChannel("MyNotifictaions","MyNotifictaions", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager=getSystemService(NotificationManager.class);
-            assert manager != null;
-            manager.createNotificationChannel(channel);
-        }
-
         setContentView(R.layout.activity_main);
 
         FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();

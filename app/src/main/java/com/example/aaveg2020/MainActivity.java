68d1c81d2e7ea1646426a77e59f849cb2c4a5405
TejusR <rameshtejus@gmail.com>
2020-01-31 19:23:24 +0530

    [feat] Home Fragment

--- app/src/main/java/com/example/aaveg2020/MainActivity.java
+++ app/src/main/java/com/example/aaveg2020/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.example.aaveg2020.Home.HomeFragment;
 import com.example.aaveg2020.Scoreboard.ScoreboardFragment;
 import com.example.aaveg2020.login.LoginActivity;
 
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_framelayout,new ScoreboardFragment());
+        fragmentTransaction.replace(R.id.main_framelayout,new HomeFragment());
         fragmentTransaction.commit();
     }
 

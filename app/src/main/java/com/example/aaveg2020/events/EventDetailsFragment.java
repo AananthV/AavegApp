2b54da8aefccb8771a4cf63fd823d92eab67cd81
Eswaraprasadp <peswar1701@gmail.com>
2020-02-04 23:18:59 +0530

    Move res/navigation/mipmap assets to res/mipmap

--- app/src/main/java/com/example/aaveg2020/events/EventDetailsFragment.java
+++ app/src/main/java/com/example/aaveg2020/events/EventDetailsFragment.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
